440776c8be8989aafdc706b86c63181362747ed9
Gabriela Judith Hernandez Sipaque <159089345+saddick-bitch@users.noreply.github.com>
2025-03-24 19:34:55 -0600

    No se que hago con mi vida
    ponganos 10 inge

---
+++ Agenda_De_contactos/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InventorySystem.Models
+{
+    // Clase base para entidades con identificador único
+    public abstract class BaseEntity
+    {
+        public Guid Id { get; protected set; } = Guid.NewGuid();
+        public DateTime CreatedAt { get; protected set; } = DateTime.UtcNow;
+    }
