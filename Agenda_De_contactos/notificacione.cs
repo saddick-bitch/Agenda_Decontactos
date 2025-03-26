7b943259f8ad91e3d2f8cc7a78bce285ba1340f3
Alex <168040901+Al3X256@users.noreply.github.com>
2025-03-25 21:00:12 -0600

    Denis Reina
    Denis Reina

---
+++ Agenda_De_contactos/notificacione.cs
@@ -0,0 +1,25 @@
+﻿namespace EL
+{
+    public class Notificacion
+    {
+        public int Id { get; set; }
+        public string Mensaje { get; set; }
+        public DateTime FechaEnvio { get; set; }
+        public string Destinatario { get; set; }
+
+        public Notificacion(int id, string mensaje, DateTime fechaEnvio, string destinatario)
+        {
+            Id = id;
+            Mensaje = mensaje;
+            FechaEnvio = fechaEnvio;
+            Destinatario = destinatario;
+        }
+
+        public override string ToString()
+        {
+            return $"[{FechaEnvio}] Para: {Destinatario} - {Mensaje}";
+        }
+    }
+}
+
+}
