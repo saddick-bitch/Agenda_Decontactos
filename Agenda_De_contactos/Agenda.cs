0802cc8a5c7fd3ac1c3b44157ef8a85a4cf8f583
Wiliam Escobar Cardoza <ec0801032024@unab.edu.sv>
2025-03-25 20:19:25 -0600

    William Escobar
    Clase Agenda

---
+++ Agenda_De_contactos/Agenda.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+using System;
+
+
+}
+public class Agenda
+{
+    private List<Contacto> contactos;
+
+    public Agenda()
+    {
+        contactos = new List<Contacto>();
+    }
+
+    public void AgregarContacto(Contacto contacto)
+    {
+        if (!contactos.Any(c => c.Telefono == contacto.Telefono)) 
+            contactos.Add(contacto);
+        else
+            throw new Exception("El contacto con este teléfono ya existe.");
+    }
+
+    public void EliminarContacto(Guid id)
+    {
+        var contacto = contactos.FirstOrDefault(c => c.Id == id);
+        if (contacto != null)
+            contactos.Remove(contacto);
+        else
+            throw new Exception("Contacto no encontrado.");
+    }
+
+    public Contacto BuscarPorNombre(string nombre)
+    {
+        return contactos.FirstOrDefault(c => c.Nombre.Equals(nombre, StringComparison.OrdinalIgnoreCase));
+    }
+
+    public Contacto BuscarPorTelefono(string telefono)
+    {
+        return contactos.FirstOrDefault(c => c.Telefono == telefono);
+    }
+
+    public List<Contacto> ListarContactos()
+    {
+        return contactos;
+    }
+}
